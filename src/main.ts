ec03cd8c83333f978af9795fabb488e86ed01e2e
Hasan Carrizo <hasan.carrizo2002@gmail.com>
2025-08-05 18:36:10 -0300

    feat: configure cors for production

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://blog-demoapp.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
